9fdea6dbaa3e894c0b44b761fb65bbb16032c61d
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-11-22 17:23:05 +0100

    Generates Spring Boot project

---
+++ src/main/java/com/followinsider/FollowInsiderApplication.java
@@ -0,0 +1,13 @@
+package com.followinsider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowInsiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowInsiderApplication.class, args);
+	}
+
+}
